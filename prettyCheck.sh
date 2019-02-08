c68b4ffcee47ee4a8281b9be59d3a33d1e23a4d4
Song Yang <song.yang3@mail.mcgill.ca>
2019-02-08 16:17:10 -0500

    Proper path to piping in pretty checker

--- prettyCheck.sh
+++ prettyCheck.sh
@@ -59,7 +59,7 @@ for sub in $(ls programs)
 do
 	for program in $(ls programs/$sub/valid/ | egrep '*.min')
 	do
-		cat $program | ./src/minic pretty > ./pretty/$sub/$program
+		cat ./src/minic/$program | ./src/minic pretty > ./pretty/$sub/$program
 	done
 done
 
@@ -72,7 +72,7 @@ for sub in $(ls pretty)
 do
 	for program in $(ls pretty/$sub/ | egrep '*.min')
 	do
-		cat $program | ./src/minic pretty > ./pretty/$sub/compare/$program
+		cat ./src/minic/$program | ./src/minic pretty > ./pretty/$sub/compare/$program
 	done
 done
 
