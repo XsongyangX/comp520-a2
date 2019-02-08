92a49961aa0a065e530395579f3cdbc76ca32f4d
Song Yang <song.yang3@mail.mcgill.ca>
2019-02-08 16:47:24 -0500

    Fix undefined function

--- prettyCompare.py
+++ prettyCompare.py
@@ -11,6 +11,13 @@ import os
 # get argument to decide to remove the directories or not
 if len(sys.argv) != 2:
 	raise Exception("Needs exactly 1 argument: destroy | keep")
+
+def compare(prettyPrintFile, printPrintFile2):
+
+	for (line1, line2) in zip(prettyPrintFile, prettyPrintFile2):
+		
+		if line1 != line2:
+			print(line1, line2)
 	
 # compare files
 for folder in os.listdir("pretty"):
@@ -32,12 +39,7 @@ for folder in os.listdir("pretty"):
 					print("identity violated : " + str(sourceFile))
 					compare(prettyPrint, prettyPrint2)
 					
-def compare(prettyPrintFile, printPrintFile2):
 
-	for (line1, line2) in zip(prettyPrintFile, prettyPrintFile2):
-		
-		if line1 != line2:
-			print(line1, line2)
 					
 # destroy pretty folder if wanted
 if (sys.argv[1] == "destroy"):
