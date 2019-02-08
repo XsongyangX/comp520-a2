1e60c27256ee75f32b03e6f9492233e0b72ddee0
Song Yang <song.yang3@mail.mcgill.ca>
2019-02-08 15:58:25 -0500

    Initial pretty compare python file

---
+++ prettyCompare.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+assumes that the program to check are in the folders
+created by prettyCheck.sh
+"""
+
+import sys
+import os
+
+# get argument to decide to remove the directories or not
+if len(sys.argv) != 2:
+	raise Exception("Needs exactly 1 argument: destroy | keep")
+	
+# compare files
+for folder in os.listdir("pretty"):
+
+	for subfolder in os.listdir(os.path.join("pretty/", folder))
+		print("in" + str(subfolder))
