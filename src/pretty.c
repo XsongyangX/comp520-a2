f95fd73bba6cb3a7b1acf88913cba42cf39aecda
Song Yang <songyang1218@gmail.com>
2019-02-07 23:39:16 -0500

    Pretty printer for control flow nodes

--- src/pretty.c
+++ src/pretty.c
@@ -1,28 +1,63 @@
 #include <stdio.h>
 #include "pretty.h"
 
-void prettyProgram(Program *program) {
+void prettyProgram(Program *p) {
     
-	# null case
-	if (program == NULL) {
+	// null case
+	if (p == NULL) {
 		printf("\n");
 		return;
 	}
 	
-	switch (program->kind) {
+	switch (p->kind) {
 		
 		case k_programKindControlFlow:
 			
-			prettyControlFlow(program->content.controlFlow.controlFlow);
-			prettyProgram(program->content.controlFlow.next);
+			prettyControlFlow(p->content.controlFlow.controlFlow);
+			prettyProgram(p->content.controlFlow.next);
 			
 			break;
 			
 		case k_programKindStatement:
 			
-			prettyStatement(program->content.statement.statement);
-			prettyProgram(program->content.statement.next);
+			prettyStatement(p->content.statement.statement);
+			prettyProgram(p->content.statement.next);
 			
 			break;
 	}
+}
+
+void prettyControlFlow(ControlFlow *cf) {
+	
+	// null case
+	if (cf == NULL) {
+		printf("\n");
+		return;
+	}
+	
+	switch (cf->kind){
+		
+		case k_controlFlowKindIf:
+		case k_controlFlowKindElseIf:
+			
+			prettyExpression(cf->content.continuing.condition);
+			prettyProgram(cf->content.continuing.block);
+			prettyControlFlow(cf->content.continuing.elsePart);
+			
+			break;
+			
+		case k_controlFlowKindElse:
+			
+			prettyProgram(cf->content.block);
+			
+			break;
+			
+		case k_controlFlowKindWhile:
+			
+			prettyExpression(cf->content.whileLoop.condition);
+			prettyProgram(cf->content.whileLoop.block);
+			
+			break;
+	}
+	
 }
