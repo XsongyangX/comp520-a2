e3b559a3e9a1896d9e549531043a7736d498a529
Song Yang <songyang1218@gmail.com>
2019-02-10 12:30:40 -0500

    Add emitters from AST

--- src/emit.c
+++ src/emit.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "emit.h"
+#include "type.h"
 
 void emitToFile(char *fileName) {
 	
--- src/emit.h
+++ src/emit.h
@@ -1,6 +1,16 @@
 #ifndef EMIT_H
 #define EMIT_H
 
+#include "tree.h"
+
+FILE *targetFile;
+
 void emitToFile(char *fileName);
+
+void emitProgramStart(Program *root);
+void emitProgram(Program *p);
+void emitControlFlow(ControlFlow *cf);
+void emitStatement(Statement *s);
+void emitExpression(Expression *e);
 
 #endif 
