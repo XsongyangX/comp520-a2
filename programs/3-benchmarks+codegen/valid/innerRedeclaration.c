0d833a496d3e604a3421432cc45b9526388bb359
Song Yang <songyang1218@gmail.com>
2019-02-10 18:51:10 -0500

    Remove .c files in programs folder

--- programs/3-benchmarks+codegen/valid/innerRedeclaration.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-void main(){
-	char *sX;
-	if (1){
-		int sX = 3;
-	}
-	}
--- programs/3-benchmarks+codegen/valid/timesIntIntFloat.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-void main(){
-	int aX;
-	int bX;
-	float returnX = (aX*bX);
-}
